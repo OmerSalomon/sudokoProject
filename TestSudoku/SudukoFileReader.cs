0d9ee8ff366e9c700af037f81c465ef2f11fa0f3
OmerSalomon <102198073+OmerSalomon@users.noreply.github.com>
2024-02-19 16:09:07 +0200

    update dec

--- TestSudoku/SudukoFileReader.cs
+++ TestSudoku/SudukoFileReader.cs
@@ -9,14 +9,31 @@ namespace sudoko_project.Test
     internal class SudukoFileReader
     {
         public const string SUDOKOS_FILE_RELATIVE_PATH = @"sudoko_for_tests\sudokos.txt";
-        public static List<string> ReadSudokosStringsFromFile()
+
+
+        /// <summary>
+        /// Reads Sudoku puzzles from a file, with each puzzle represented as a single line string.
+        /// The method navigates from the current application domain's base directory up to the project's root directory,
+        /// where it expects to find the Sudoku puzzles file. Each line in the file is treated as a separate Sudoku puzzle.
+        /// </summary>
+        /// <returns>
+        /// A List of strings, where each string represents a full Sudoku puzzle.
+        /// If an error occurs during file reading, the method prints the error message to the console and returns null.
+        /// </returns>
+        /// <remarks>
+        /// The file path to the Sudoku puzzles is constructed dynamically using the project's root directory and a
+        /// relative path defined by SUDOKOS_FILE_RELATIVE_PATH.
+        /// Adjust the number of directory levels to navigate up ("..") based on the actual project structure.
+        /// </remarks>
+        /// <exception cref="System.IO.FileNotFoundException">Thrown if the Sudoku puzzles file is not found.</exception>
+        /// <exception cref="System.Exception">General exception thrown for any other error encountered during file reading.</exception>
+        public static List<string> ReadSudokosStringsFromFile(string relativePath)
         {
 
             string basePath = AppDomain.CurrentDomain.BaseDirectory;
             // Navigate up to the project root directory (adjust the number of ".." as needed)
             string projectRootPath = Path.GetFullPath(Path.Combine(basePath, @"..\..\..\"));
-            string relativeFilePath = SUDOKOS_FILE_RELATIVE_PATH;
-            string filePath = Path.Combine(projectRootPath, relativeFilePath);
+            string filePath = Path.Combine(projectRootPath, relativePath);
 
             try
             {
